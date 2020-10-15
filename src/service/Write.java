b9b1dd391b3edfd23c0d194490e3b4bfd99c8dc2
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-10-15 11:26:08 +0700

    Sdfds

--- src/service/Write.java
+++ src/service/Write.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Write {
     public static void write() {
         try {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File("bookPhone")));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File("bookPhone.dat")));
             objectOutputStream.writeObject(objectOutputStream);
             objectOutputStream.close();
         } catch (IOException e) {
